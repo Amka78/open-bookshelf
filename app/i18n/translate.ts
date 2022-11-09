e48e2cc5348cc33ad550da728c1d00e5191cc813
Amemiya Hikaru <amemiyahikaru@bysrsystemdevelopment.com>
2022-11-10 01:00:10 +0900

    Translate support for variable length arguments.

--- app/i18n/translate.ts
+++ app/i18n/translate.ts
@@ -1,6 +1,9 @@
 import i18n from "i18n-js"
 import { TxKeyPath } from "./i18n"
 
+export type MessageKey =
+  | TxKeyPath
+  | { key: TxKeyPath; restParam?: (TxKeyPath | { key: TxKeyPath | string; translate: boolean })[] }
 /**
  * Translates text.
  *
@@ -25,6 +28,32 @@ import { TxKeyPath } from "./i18n"
  * // => "Hello world!"
  * ```
  */
-export function translate(key: TxKeyPath, options?: i18n.TranslateOptions) {
-  return i18n.t(key, options)
+export function translate(message: MessageKey, options?: i18n.TranslateOptions) {
+  let currentLocaleValue = null
+
+  if (typeof message === "string") {
+    currentLocaleValue = i18n.t(message, options)
+  } else if (typeof message !== "undefined") {
+    currentLocaleValue = i18n.t(message.key, options)
+    if (currentLocaleValue && message.restParam) {
+      for (let i = 0; message.restParam.length > i; i++) {
+        let settingValue
+
+        if (typeof message.restParam[i] === "string") {
+          const key = message.restParam[i] as string
+          settingValue = i18n.t(key, options)
+        } else {
+          const object = message.restParam[i] as { key: string; translate: boolean }
+          if (object.translate) {
+            settingValue = i18n.t(object.key, options)
+          } else {
+            settingValue = object.key
+          }
+        }
+        currentLocaleValue = currentLocaleValue.replace("{" + i + "}", settingValue)
+      }
+    }
+  }
+  return currentLocaleValue
+
 }
