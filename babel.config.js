7d00782d847d0e898f0ded9e0ab7aa410e0c6363
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2023-12-21 02:55:39 +0900

    Fix to work unittest.

--- babel.config.js
+++ babel.config.js
@@ -10,31 +10,13 @@ const plugins = [
   //["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
 ]
 
-const vanillaConfig = {
-  presets: ["module:metro-react-native-babel-preset"],
-  env: {
-    production: {},
-  },
-  plugins,
-}
-
-const expoConfig = {
-  presets: ["babel-preset-expo"],
-  env: {
-    production: {},
-  },
-  plugins,
-}
-
-let isExpo = false
-try {
-  const Constants = require("expo-constants")
-  // True if the app is running in an `expo build` app or if it's running in Expo Go.
-  isExpo =
-    Constants.executionEnvironment === "standalone" ||
-    Constants.executionEnvironment === "storeClient"
-} catch {}
-
-const babelConfig = isExpo ? expoConfig : vanillaConfig
-
-module.exports = babelConfig
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {},
+    },
+    plugins,
+  };
+};
