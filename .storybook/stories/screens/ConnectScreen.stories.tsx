e4ad050d97dbc305ad06053acc343bfb948dba2e
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-01-19 02:46:00 +0900

    Fix dependencies.

--- .storybook/stories/screens/ConnectScreen.stories.tsx
+++ .storybook/stories/screens/ConnectScreen.stories.tsx
@@ -1,4 +1,3 @@
-import { ApiError } from "@/models/exceptions/Exceptions"
 import { ConnectScreen } from "@/screens/ConnectScreen/template/ConnectScreen"
 import { delay } from "@/utils/delay"
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react-native"
@@ -6,7 +5,7 @@ import React from "react"
 
 import { ScreenContainer } from "./ScreenContainer"
 
-export default {
+export default ({
   component: ConnectScreen,
   decorators: [
     (Story) => (
@@ -15,24 +14,13 @@ export default {
       </ScreenContainer>
     ),
   ],
-} as ComponentMeta<typeof ConnectScreen>
+} as ComponentMeta<typeof ConnectScreen>)
 type ConnectScreenStory = ComponentStoryObj<typeof ConnectScreen>
 export const Basic: ConnectScreenStory = {
-  args: {
-    onConnectPress: async (data) => {
-      console.log(data)
-      await delay(1000)
-    },
-  },
+  args: {},
 }
 export const CanNotConnect: ConnectScreenStory = {
   args: {
     baseUrl: "http://192.168.1.XX:XXXX",
-    onConnectPress: async (data) => {
-      throw new ApiError({
-        errorTx: "errors.canNotConnect",
-        descriptionTx: "errors.canNotConnectDescription",
-      })
-    },
   },
 }
