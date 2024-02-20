ec0b8bfda698f07eb52c3827c1c8ff7ecdb3ce91
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-20 11:25:14 +0900

    Add VStack story.

---
+++ app/components/VStack/VStack.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Box, VStack } from "@/components"
+
+import { ComponentHolder } from "../../../.storybook/stories/ComponentHolder"
+
+export default {
+  title: "VStack",
+  component: VStack,
+  parameters: {
+    notes: `Place component vertically`,
+  },
+} as Meta<typeof VStack>
+
+type StoryProps = StoryObj<typeof VStack>
+
+export const Basic: StoryProps = {
+  decorators: [
+    (Story) => (
+      <ComponentHolder>
+        <Story/>
+      </ComponentHolder>
+    ),
+  ],
+  args: {
+    children:  (<>
+          <Box width={"$32"} height={"$32"} backgroundColor="blue"/>
+          <Box width={"$32"} height={"$32"} backgroundColor="red"/>
+          <Box width={"$32"} height={"$32"} backgroundColor="green"/></>)
+  }
+}
