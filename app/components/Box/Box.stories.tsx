540fd303e784715662804d79abe9d6d53554b11f
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-20 13:22:24 +0900

    Add Box Story.

---
+++ app/components/Box/Box.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Box, VStack } from "@/components"
+
+import { ComponentHolder } from "../../../.storybook/stories/ComponentHolder"
+
+export default {
+  title: "Box",
+  component: Box,
+  args: {
+    height: "$32",
+    width: "$32",
+    backgroundColor: "red"
+  },
+  parameters: {
+    notes: `Standard container`,
+  },
+  decorators: [
+    (Story) => (
+      <ComponentHolder>
+        <Story/>
+      </ComponentHolder>
+    ),
+  ],
+} as Meta<typeof Box>
+
+type StoryProps = StoryObj<typeof Box>
+
+export const Basic: StoryProps = {}
