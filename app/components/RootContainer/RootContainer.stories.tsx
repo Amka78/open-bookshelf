8738edfefc23d891983f15c759d6b4d32745935e
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-21 10:26:52 +0900

    Add RootContainer story.

---
+++ app/components/RootContainer/RootContainer.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Box, RootContainer, Text } from "@/components"
+
+import { ComponentHolder } from "../../../.storybook/stories/ComponentHolder"
+
+export default {
+  title: "RootContainer",
+  component: RootContainer,
+  parameters: {
+    notes: `Convergence-enabled screen containers.`,
+  },
+  decorators: [
+    (Story) => (
+      <ComponentHolder>
+        <Story />
+      </ComponentHolder>
+    ),
+  ],
+} as Meta<typeof RootContainer>
+
+type StoryProps = StoryObj<typeof RootContainer>
+
+export const Basic: StoryProps = {
+  args: {
+    children: <Box flex={1} backgroundColor="blue">
+      <Text>{"RootContainer Test"}</Text>
+    </Box>
+  }
+}
