ef0ac86f849d9287626aeaa16fff4dc48492fd7f
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-21 13:24:01 +0900

    Add Style chnage function.

--- .storybook/stories/ComponentHolder.tsx
+++ .storybook/stories/ComponentHolder.tsx
@@ -3,14 +3,18 @@ import { Platform } from "react-native"
 import { Markdown } from "@storybook/blocks"
 import { GluestackUIProvider } from "@gluestack-ui/themed"
 import { config } from "@gluestack-ui/config"
+import { ComponentProps } from "react"
 export type ComponentHolderProps = {
   children: React.ReactNode
   markdown?: string
-}
-export function ComponentHolder(props: ComponentHolderProps) {
+} & ComponentProps<typeof Box>
+export function ComponentHolder({ alignItems = "flex-start", justifyContent = "flex-start", ...restProps }: ComponentHolderProps) {
+
+  const props = { alignItems, justifyContent, ...restProps }
   return (
     <GluestackUIProvider config={config}>
       <Box
+        {...props}
         flex={1}
         alignItems={"flex-start"}
         justifyContent={"flex-start"}
