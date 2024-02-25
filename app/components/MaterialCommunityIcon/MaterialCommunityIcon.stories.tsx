9dd826d0f6d3444ee33a1be58faf25fded99290a
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-26 08:34:25 +0900

    Add MaterialCommunityIcon story.

---
+++ app/components/MaterialCommunityIcon/MaterialCommunityIcon.stories.tsx
@@ -0,0 +1,66 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { MaterialCommunityIcon } from "@/components"
+
+import { ComponentHolder } from "../../../.storybook/stories/ComponentHolder"
+
+export default {
+  title: "MaterialCommunityIcon",
+  component: MaterialCommunityIcon,
+  parameters: {
+    notes: `OpenBookShelf's common icons`,
+  },
+  args: {
+    name: "newspaper"
+  },
+  decorators: [
+    (Story) => (
+      <ComponentHolder>
+        <Story />
+      </ComponentHolder>
+    ),
+  ],
+} as Meta<typeof MaterialCommunityIcon>
+
+type StoryProps = StoryObj<typeof MaterialCommunityIcon>
+
+export const Basic: StoryProps = {}
+export const SizeMdMinus: StoryProps = {
+  args: {
+    iconSize: "md-"
+  }
+}
+export const SizeSM: StoryProps = {
+  args: {
+    iconSize: "sm"
+  }
+}
+export const SizeSMMinus: StoryProps = {
+  args: {
+    iconSize: "sm-"
+  }
+}
+export const VariantStaggerChild: StoryProps = {
+  args: {
+    variant: "staggerChild"
+  }
+}
+export const VariantStaggerRoot: StoryProps = {
+  args: {
+    variant: "staggerRoot"
+  }
+}
+export const Rotate90: StoryProps = {
+  args: {
+    rotate: "90"
+  }
+}
+export const Rotate180: StoryProps = {
+  args: {
+    rotate: "180"
+  }
+}
+export const Rotate270: StoryProps = {
+  args: {
+    rotate: "270"
+  }
+}
