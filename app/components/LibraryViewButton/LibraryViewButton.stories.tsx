82cebe10a6235f39de523004cc6b4caf5ec38579
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-26 12:25:24 +0900

    Add LibraryViewButton Story.

---
+++ app/components/LibraryViewButton/LibraryViewButton.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { LibraryViewButton } from "@/components"
+
+import { ComponentHolder } from "../../../.storybook/stories/ComponentHolder"
+
+export default {
+  title: "LibraryViewButton",
+  component: LibraryViewButton,
+  parameters: {
+    notes: `Toggles the View display method.`,
+  },
+  args: {
+    mode: "gridView"
+  },
+  argTypes: {
+    onPress: { action: "Press LibraryViewButton." }
+  },
+  decorators: [
+    (Story) => (
+      <ComponentHolder>
+        <Story />
+      </ComponentHolder>
+    ),
+  ],
+} as Meta<typeof LibraryViewButton>
+
+type StoryProps = StoryObj<typeof LibraryViewButton>
+
+export const GridView: StoryProps = {}
+export const ViewList: StoryProps = {
+  args: {
+    mode: "viewList"
+  }
+}
