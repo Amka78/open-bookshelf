a7deaa2a2d6ae9c183eca89a123aa6b98db601d3
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-02-29 16:58:30 +0900

    Improve Checkbox component.

--- app/components/Checkbox/Checkbox.tsx
+++ app/components/Checkbox/Checkbox.tsx
@@ -6,16 +6,17 @@ import {
   CheckboxIcon,
   CheckIcon,
 } from "@gluestack-ui/themed"
-import React, { ComponentProps, forwardRef } from "react"
+import React, { ComponentProps, forwardRef, Ref } from "react"
+import { View } from "react-native"
 
 export type ICheckboxProps = ComponentProps<typeof Template> & {
   tx?: MessageKey
 }
-export const Checkbox = forwardRef((props: ICheckboxProps, ref) => {
+export const Checkbox = forwardRef((props: ICheckboxProps, ref: Ref<View>) => {
   return (
-    <Template {...props} ref={ref}>
+    <Template {...props} ref={ref} >
       <CheckboxLabel>{props.tx ? translate(props.tx) : props.children}</CheckboxLabel>
-      <CheckboxIndicator>
+      <CheckboxIndicator marginLeft={"$0.5"}>
         <CheckboxIcon as={CheckIcon} />
       </CheckboxIndicator>
     </Template>
