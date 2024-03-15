b9f113509a09701a93b3ab1a184c7312b60d6465
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2024-03-15 11:23:49 +0900

    Add Mobile DateTimePicker.

--- app/components/DateTimePicker/DateTimePicker.tsx
+++ app/components/DateTimePicker/DateTimePicker.tsx
@@ -1,9 +1,44 @@
-import { View } from "react-native"
+import Picker from "@react-native-community/datetimepicker"
+import { HStack, IconButton, Text } from "@/components"
+import { useState } from "react"
+import { formatDate } from "@/utils/formatDate"
+import { parseISO } from "date-fns"
 
 export type DateTimePickerProps = {
-  value: Date
+  value: string
   onChange: (date: string) => void
+  dateFormat: string
 }
-export function DateTimePicker() {
-  return <View></View>
+export function DateTimePicker(props: DateTimePickerProps) {
+  const [show, setShow] = useState(false)
+  console.log(props.value)
+  console.log(props.dateFormat)
+  return (
+    <>
+      <HStack>
+        <Text flex={1}>
+          {props.value ? formatDate(parseISO(props.value), props.dateFormat) : undefined}
+        </Text>
+        <IconButton
+          name={"calendar"}
+          onPress={() => {
+            setShow(true)
+          }}
+        />
+      </HStack>
+      {show && (
+        <Picker
+          testID="dateTimePicker"
+          value={props.value ? new Date(props.value) : new Date()}
+          mode={"date"}
+          is24Hour={true}
+          onChange={(_, selectedDate) => {
+            if (props.onChange) {
+              props.onChange(selectedDate.toISOString())
+            }
+          }}
+        />
+      )}
+    </>
+  )
 }
