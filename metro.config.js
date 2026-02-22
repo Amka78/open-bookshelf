9eb50d0c2af2098f34b1c73257cf7297990f7c6d
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2026-02-23 00:30:48 +0900

    previewがビルドできるよう修正。

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const path = require("path")
+
+const axiosBrowserEntry = path.resolve(__dirname, "node_modules/axios/dist/browser/axios.cjs")
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
 config.transformer.getTransformOptions = async () => ({
   transform: {
     experimentalImportSupport: false,
     inlineRequires: true,
   },
-});
+})
 
-const { server } = config;
+const { server } = config
 
 config.server = {
   ...server,
@@ -24,16 +27,40 @@ config.server = {
           target: "http://192.168.1.11:8081",
           changeOrigin: true,
           pathRewrite: { "^/test-url": "" },
-        });
-        return proxy(req, res, next);
+        })
+        return proxy(req, res, next)
       }
-      return middleware(req, res, next);
-    };
+      return middleware(req, res, next)
+    }
   },
-};
+}
 
-config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs", "cjs"];
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs", "cjs"]
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  axios: axiosBrowserEntry,
+}
+
+const defaultResolveRequest = config.resolver.resolveRequest
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    moduleName === "axios" ||
+    moduleName === "axios/dist/node/axios.cjs" ||
+    moduleName === "axios/dist/node/axios"
+  ) {
+    return {
+      type: "sourceFile",
+      filePath: axiosBrowserEntry,
+    }
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform)
+  }
+
+  return context.resolveRequest(context, moduleName, platform)
+}
 
 // pdfjs-dist は npm から削除し、CDN 経由でロードするため特別な設定は不要
 
-module.exports = config;
+module.exports = config
