1e8b5bbf5bc67c4a2c086cd2af0bbfe7efd418e5
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2026-04-25 23:44:36 +0900

    chore(release): v0.0.1

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,7 @@ export default {
   expo: {
     name: `OpenBookShelf${appNameSuffix}`,
     slug: "OpenBookShelf",
-    version: "1.0.0",
+    version: "0.0.1",
     orientation: "default",
     icon: "./assets/images/app-icon.png",
     experiments: {
